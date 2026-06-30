8de6403db9e0df1717e8ffbff74939eaeaa10ca7
sobbify <harshitbery@proton.me>
2026-06-30 08:06:44 +0000

    fix

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_all, create_engine, Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import datetime
